742c7e4b93b91b1a1dff2c375c5ba6c858f269a7
Abdulqadir000 <aabdulqadir300@gmail.com>
2025-01-26 08:32:21 +0500

    final full hackathon

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
+// import express from "express";
+// import bodyParser from "body-parser";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import authRoutes from "./routes/authRoutes.js";
+// import connectDB from "./lib/DBconnect.js";
+// dotenv.config();
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(bodyParser.json());
+
+// // Connect to DB
+// connectDB();
+
+// // Routes
+// app.use("/api/auth", authRoutes);
+
+// // Start Server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 import express from "express";
-import bodyParser from "body-parser";
 import cors from "cors";
+import userRouter from "./routes/users.js";
+import authRouter from "./routes/auth.js";
+import loanRequestRouter from "./routes/loanRequest.js";
+import connectDB from "./lib/DBconnect.js";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes.js";
-import connectDB from "./lib/connectDB.js";
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
-// Middleware
+app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json());
+app.use("/users", userRouter);
+app.use("/auth", authRouter);
+app.use("/loan", loanRequestRouter);
 
-// Connect to DB
-connectDB();
-
-// Routes
-app.use("/api/auth", authRoutes);
-
-// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ models/Appointment.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const appointmentSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  applicationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Application",
+    required: true,
+  },
+  appointmentDate: { type: Date, required: true },
+  appointmentTime: { type: String, required: true },
+  location: { type: String, required: true },
+  slotStatus: {
+    type: String,
+    enum: ["Scheduled", "Completed", "Cancelled"],
+    default: "Scheduled",
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const Appointment = mongoose.model("Appointment", appointmentSchema);
+export default Appointment;
